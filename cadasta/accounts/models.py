61b1a9a4355e59f7888f5ae87905029c874e2641
Parthvi Vala <parthvi.vala@gmail.com>
2017-06-13 23:32:18 +0530

    Added verificationdevice, modified User model, added related migrations

--- cadasta/accounts/models.py
+++ cadasta/accounts/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timezone, timedelta
 from django.conf import settings
 from django.db import models
 from django.dispatch import receiver
@@ -10,16 +9,17 @@ import django.contrib.auth.base_user as auth_base
 from allauth.account.signals import password_changed, password_reset
 from tutelary.models import Policy
 from tutelary.decorators import permissioned_model
+from django_otp.models import Device
+from django_otp.oath import TOTP
+from django_otp.util import random_hex, hex_validator
+from binascii import unhexlify
+from .gateways import TwilioSMS
 
 from simple_history.models import HistoricalRecords
 from .manager import UserManager
 
 
 PERMISSIONS_DIR = settings.BASE_DIR + '/permissions/'
-
-
-def now_plus_48_hours():
-    return datetime.now(tz=timezone.utc) + timedelta(hours=48)
 
 
 def abstract_user_field(name):
@@ -37,7 +37,9 @@ class User(auth_base.AbstractBaseUser, auth.PermissionsMixin):
     is_active = abstract_user_field('is_active')
     date_joined = abstract_user_field('date_joined')
     email_verified = models.BooleanField(default=False)
-    verify_email_by = models.DateTimeField(default=now_plus_48_hours)
+    # verify_email_by = models.DateTimeField(default=now_plus_48_hours)
+    phone = models.CharField(max_length=12, unique=True, blank=True)
+    phone_verified = models.BooleanField(default=False)
     change_pw = models.BooleanField(default=True)
 
     objects = UserManager()
@@ -45,7 +47,7 @@ class User(auth_base.AbstractBaseUser, auth.PermissionsMixin):
     history = HistoricalRecords()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'full_name']
+    REQUIRED_FIELDS = ['email' or 'phone', 'full_name']
 
     class Meta:
         ordering = ('username',)
@@ -70,7 +72,8 @@ class User(auth_base.AbstractBaseUser, auth.PermissionsMixin):
                        ' full_name={obj.full_name}'
                        ' email={obj.email}'
                        ' email_verified={obj.email_verified}'
-                       ' verify_email_by={obj.verify_email_by}>')
+                       ' phone={obj.phone}'
+                       ' phone_verified={obj.phone_verified}>')
         return repr_string.format(obj=self)
 
     def get_display_name(self):
@@ -106,3 +109,48 @@ def password_changed_reset(sender, request, user, **kwargs):
         [user.email],
         fail_silently=False
     )
+
+
+def default_key():
+    return random_hex(20)
+
+
+class VerificationDevice(Device):
+    secret_key = models.CharField(
+        max_length=40,
+        default=default_key,
+        validators=[hex_validator],
+        help_text="Hex-encoded secret key to generate totp tokens.")
+    user = models.ForeignKey(User)
+
+    step = getattr(settings, 'TOTP_TOKEN_VALIDITY')
+    digits = getattr(settings, 'TOTP_DIGITS')
+
+    class Meta(Device.Meta):
+        verbose_name = "Verification Device"
+
+    @property
+    def bin_key(self):
+        return unhexlify(self.secret_key.encode())
+
+    def totp_obj(self):
+        totp = TOTP(key=self.bin_key, step=self.step, digits=self.digits)
+        return totp
+
+    def generate_token(self):
+        totp = self.totp_obj()
+        token = totp.token()
+        message = "Your Cadasta Token is %s. It is valid for %s minutes." % (
+            token, getattr(settings, 'TOTP_TOKEN_VALIDITY') * 60)
+
+        twilio_obj = TwilioSMS()
+        twilio_obj.send_sms(phone=self.user.phone, message=message)
+
+    def verify_token(self, token):
+        totp = self.totp_obj()
+        if totp.verify(token, tolerance=0):
+            self.user.phone_verified = True
+        else:
+            self.user.phone_verified = False
+
+        return self.user.phone_verified
